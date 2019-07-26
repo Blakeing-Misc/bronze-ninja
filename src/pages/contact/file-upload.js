06fe3410f373940c358a2d9042b660a1b264afbd
Blake Ingenthron <blakeingenthron@gmail.com>
2019-07-26 12:35:58 -0500

    Update File-Upload-2

--- src/pages/contact/file-upload.js
+++ src/pages/contact/file-upload.js
@@ -12,97 +12,71 @@ function encode(data) {
 	return formData;
 }
 
-export default class Contact extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+export default function Contact() {
+	const [state, setState] = React.useState({});
 
-	handleChange = e => {
-		this.setState({ [e.target.name]: e.target.value });
+	const handleChange = e => {
+		setState({ [e.target.name]: e.target.value });
 	};
 
-	handleAttachment = e => {
-		this.setState({ [e.target.name]: e.target.files[0] });
+	const handleAttachment = e => {
+		setState({ [e.target.name]: e.target.files[0] });
 	};
 
-	handleSubmit = e => {
+	const handleSubmit = e => {
 		e.preventDefault();
 		const form = e.target;
-		fetch("/?no-cache=1", {
+		fetch("/", {
 			method: "POST",
 			body: encode({
 				"form-name": form.getAttribute("name"),
-				...this.state
+				...state
 			})
 		})
 			.then(() => navigate(form.getAttribute("action")))
 			.catch(error => alert(error));
 	};
 
-	render() {
-		return (
-			<Layout>
-				<section className="section">
-					<div className="container">
-						<div className="content">
-							<h1>File Upload</h1>
-							<form
-								name="file-upload"
-								method="post"
-								action="/contact/thanks/"
-								data-netlify="true"
-								data-netlify-honeypot="bot-field"
-								onSubmit={this.handleSubmit}
-							>
-								{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-								<input type="hidden" name="form-name" value="file-upload" />
-								<div hidden>
-									<label>
-										Don’t fill this out:{" "}
-										<input name="bot-field" onChange={this.handleChange} />
-									</label>
-								</div>
-								<div className="field">
-									<label className="label" htmlFor={"name"}>
-										Your name
-									</label>
-									<div className="control">
-										<input
-											className="input"
-											type={"text"}
-											name={"name"}
-											onChange={this.handleChange}
-											id={"name"}
-											required={true}
-										/>
-									</div>
-								</div>
-								<div className="field">
-									<div className="file">
-										<label className="file-label">
-											<input
-												className="file-input"
-												type="file"
-												name="attachment"
-												onChange={this.handleAttachment}
-											/>
-											<span className="file-cta">
-												<span className="file-label">Choose a file…</span>
-											</span>
-										</label>
-									</div>
-								</div>
-								<div className="field">
-									<button className="button is-link" type="submit">
-										Send
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</section>
-			</Layout>
-		);
-	}
+	return (
+		<Layout>
+			<h1>File Upload</h1>
+			<form
+				name="file-upload"
+				method="post"
+				action="/thanks/"
+				data-netlify="true"
+				data-netlify-honeypot="bot-field"
+				onSubmit={handleSubmit}
+			>
+				{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+				<input type="hidden" name="form-name" value="file-upload" />
+				<p hidden>
+					<label>
+						Don’t fill this out:{" "}
+						<input name="bot-field" onChange={handleChange} />
+					</label>
+				</p>
+				<p>
+					<label>
+						Your name:
+						<br />
+						<input type="text" name="name" onChange={handleChange} />
+					</label>
+				</p>
+				<p>
+					<label>
+						File:
+						<br />
+						<input type="file" name="attachment" onChange={handleAttachment} />
+					</label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+				<p>
+					Note: multiple file uploads are not supported by Netlify at this time.
+				</p>
+			</form>
+		</Layout>
+	);
 }
